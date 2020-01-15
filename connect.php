e5f0736af2c65e4d4eb04fcf1b575e2e11d532f4
karthik1098 <kyash0044@gmail.com>
2020-01-15 15:29:05 +0530

    this php file contains the php code for db connectivity

---
+++ connect.php
@@ -0,0 +1,35 @@
+<?php
+$username = filter_input(INPUT_POST, 'username');
+$password = filter_input(INPUT_POST, 'password');
+if (!empty($username)){
+if (!empty($password)){
+$host = "database-1.cgfawefwefweigm.xxxxxxxxxxxxx.rds.amazonaws.com";
+$dbusername = "xxxxxxxxxxxxxx";
+$dbpassword = "xxxxxxxx";
+$dbname = "db1";
+// Create connection
+$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+if (mysqli_connect_error()){
+	die('Connect Error ('. mysqli_connect_errno() .') '. mysqli_connect_error());
+	}
+else{
+	$sql = "INSERT INTO account (username, password)values ('$username','$password')";
+if ($conn->query($sql)){
+	echo "New record is inserted sucessfully";
+}
+else{
+	echo "Error: ". $sql ." ". $conn->error;
+}
+$conn->close();
+}
+}
+else{
+	echo "Password should not be empty";
+die();
+}
+}
+else{
+	echo "Username should not be empty";
+die();
+}
+?>
